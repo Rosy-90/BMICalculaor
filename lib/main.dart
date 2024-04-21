072d896a70a9ab0f303987fbc5452fd3e32f8535
Rosy Patel <rosypatel109@hotmail.com>
2024-04-21 17:28:50 +0100

    apply dark colour theme to the app

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,10 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+          primaryColor: const Color(0xFF0A0E21),
+           scaffoldBackgroundColor: const Color(0xFF0A0E21),
+      ),
       home: InputPage(),
     );
   }
