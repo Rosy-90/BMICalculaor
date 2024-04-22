02e22c6c54326d861dd4ff0758558292a332b953
Rosy Patel <rosypatel109@hotmail.com>
2024-04-22 18:54:58 +0100

    added ReusableCard class to simplify the repeated code

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -21,14 +21,70 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: const Text("BMI Calculator"),
       ),
-      body: const Center(
-        child: Text("Body Text"),
+      //body: Container(
+      body: Column(
+        children: [
+          const Expanded(
+            child: Row(
+            children: [
+               Expanded(
+                child: ReusableCard(),
+              ),
+              Expanded(
+                child: ReusableCard(),
+              ),
+            ],
+          ),
+          ),
+          Expanded(
+            child: Container(
+              height: 200,
+              margin: const EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+          ),
+          const Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(),
+                ),
+                Expanded(
+                  child: ReusableCard(),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
+      //),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  const ReusableCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 170,
+      height: 200,
+      margin: const EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        color: Colors.blue,
+        borderRadius: BorderRadius.circular(10),
+      ),
     );
   }
 }
